8f748d7c250841f636ef5d820e3047c22ebbeea1
thekcr85 <thekcr85@gmail.com>
2026-03-30 11:42:34 +0200

    Add CardNumber value object for access card identification

---
+++ src/AstraCore.AccessControl.Domain/ValueObjects/CardNumber.cs
@@ -0,0 +1,27 @@
+namespace AstraCore.AccessControl.Domain.ValueObjects;
+
+public sealed record CardNumber
+{
+    public string Value { get; }
+
+    private CardNumber(string value)
+    {
+        Value = value;
+    }
+
+    public static CardNumber Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Card number cannot be empty", nameof(value));
+
+        if (value.Length != 16)
+            throw new ArgumentException("Card number must be exactly 16 characters", nameof(value));
+
+        if (!value.All(char.IsLetterOrDigit))
+            throw new ArgumentException("Card number must contain only letters and digits", nameof(value));
+
+        return new CardNumber(value.ToUpperInvariant());
+    }
+
+    public static implicit operator string(CardNumber cardNumber) => cardNumber.Value;
+}
