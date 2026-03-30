e742bc0e49576a1005c811f368859e68e5c59044
thekcr85 <thekcr85@gmail.com>
2026-03-30 11:41:42 +0200

    Add BaseEntity as root for domain entities

---
+++ src/AstraCore.AccessControl.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+namespace AstraCore.AccessControl.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime? UpdatedAt { get; set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+}
