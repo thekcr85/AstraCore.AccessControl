9e9564d3435b042a939aa67a24f6724d1db10d49
thekcr85 <thekcr85@gmail.com>
2026-04-04 16:22:39 +0200

    Add validator for UpdateAccessPointRequest

---
+++ src/AstraCore.AccessControl.Application/Validators/UpdateAccessPointRequestValidator.cs
@@ -0,0 +1,27 @@
+using AstraCore.AccessControl.Application.DTOs.AccessPoint;
+using FluentValidation;
+
+namespace AstraCore.AccessControl.Application.Validators;
+
+public sealed class UpdateAccessPointRequestValidator : AbstractValidator<UpdateAccessPointRequest>
+{
+    public UpdateAccessPointRequestValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Location)
+            .NotEmpty().WithMessage("Location is required.")
+            .MaximumLength(200).WithMessage("Location cannot exceed 200 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters.")
+            .When(x => x.Description is not null);
+
+        RuleFor(x => x.RequiredAccessLevel)
+            .IsInEnum().WithMessage("Required access level is not valid.");
+    }
+}
