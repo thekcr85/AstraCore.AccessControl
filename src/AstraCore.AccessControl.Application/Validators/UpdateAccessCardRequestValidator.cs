bd53990ac9b1d9f4fbf372ea67698b1f9e460831
thekcr85 <thekcr85@gmail.com>
2026-04-04 16:21:15 +0200

    Add validator for UpdateAccessCardRequest

---
+++ src/AstraCore.AccessControl.Application/Validators/UpdateAccessCardRequestValidator.cs
@@ -0,0 +1,18 @@
+using AstraCore.AccessControl.Application.DTOs.AccessCard;
+using FluentValidation;
+
+namespace AstraCore.AccessControl.Application.Validators;
+
+public sealed class UpdateAccessCardRequestValidator : AbstractValidator<UpdateAccessCardRequest>
+{
+    public UpdateAccessCardRequestValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.AccessLevel)
+            .IsInEnum().WithMessage("Access level is not valid.");
+
+        RuleFor(x => x.ExpiryDate)
+            .Must(date => date > DateTime.UtcNow).WithMessage("Expiry date must be in the future.");
+    }
+}
