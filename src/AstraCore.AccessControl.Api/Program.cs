de0c19a5d2286b8480780d6a5e1c7468d940b512
thekcr85 <thekcr85@gmail.com>
2026-04-05 19:54:34 +0200

    Integrate Infrastructure layer via AddInfrastructure extension

--- src/AstraCore.AccessControl.Api/Program.cs
+++ src/AstraCore.AccessControl.Api/Program.cs
@@ -1,9 +1,11 @@
 using AstraCore.AccessControl.Application;
+using AstraCore.AccessControl.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
