c087f29236262ca6f4673e44570d04581fd200b4
thekcr85 <thekcr85@gmail.com>
2026-04-06 12:39:20 +0200

    Delete SaveChangesAsync method and make asynchronous AddAsync method sync Add

--- src/AstraCore.AccessControl.Application/Interfaces/Repositories/IAccessCardRepository.cs
+++ src/AstraCore.AccessControl.Application/Interfaces/Repositories/IAccessCardRepository.cs
@@ -7,8 +7,7 @@ public interface IAccessCardRepository
     Task<AccessCard?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
     Task<AccessCard?> GetByCardNumberAsync(string cardNumber, CancellationToken cancellationToken = default);
     Task<IReadOnlyList<AccessCard>> GetByEmployeeIdAsync(Guid employeeId, CancellationToken cancellationToken = default);
-    Task AddAsync(AccessCard card, CancellationToken cancellationToken = default);
+    void Add(AccessCard card);
     void Update(AccessCard card);
     void Remove(AccessCard card);
-    Task SaveChangesAsync(CancellationToken cancellationToken = default);
 }
