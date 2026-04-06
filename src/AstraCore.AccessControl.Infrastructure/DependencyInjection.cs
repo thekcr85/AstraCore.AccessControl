54511abb0e423713a229f48b12a1907712c3a590
thekcr85 <thekcr85@gmail.com>
2026-04-06 13:04:09 +0200

    Register repositories and UnitOfWork in DI

--- src/AstraCore.AccessControl.Infrastructure/DependencyInjection.cs
+++ src/AstraCore.AccessControl.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
+using AstraCore.AccessControl.Application.Interfaces.Repositories;
+using AstraCore.AccessControl.Application.Interfaces.UnitOfWork;
 using AstraCore.AccessControl.Infrastructure.Persistence;
+using AstraCore.AccessControl.Infrastructure.Persistence.Repositories;
+using AstraCore.AccessControl.Infrastructure.Persistence.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,15 +11,24 @@ namespace AstraCore.AccessControl.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-    {
+	public static IServiceCollection AddInfrastructure(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
 		var connectionString = configuration.GetConnectionString("DefaultConnection")
-		?? throw new InvalidOperationException(
-			"Connection string 'DefaultConnection' is not configured.");
+			?? throw new InvalidOperationException(
+				"Connection string 'DefaultConnection' is not configured.");
 
 		services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(connectionString));
+			options.UseSqlServer(connectionString));
 
-        return services;
-    }
+		services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+		services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+		services.AddScoped<IAccessCardRepository, AccessCardRepository>();
+		services.AddScoped<IAccessPointRepository, AccessPointRepository>();
+		services.AddScoped<IAccessLogRepository, AccessLogRepository>();
+
+		return services;
+	}
 }
