fbc5694746e3c619d5084f42141683745ed7854b
thekcr85 <thekcr85@gmail.com>
2026-04-06 12:41:53 +0200

    Add EF Core configuration for AccessCard entity

---
+++ src/AstraCore.AccessControl.Infrastructure/Persistence/Configurations/AccessCardConfiguration.cs
@@ -0,0 +1,40 @@
+using AstraCore.AccessControl.Domain.Entities;
+using AstraCore.AccessControl.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AstraCore.AccessControl.Infrastructure.Persistence.Configurations;
+
+internal sealed class AccessCardConfiguration : IEntityTypeConfiguration<AccessCard>
+{
+	public void Configure(EntityTypeBuilder<AccessCard> builder)
+	{
+		builder.HasKey(c => c.Id);
+
+		builder.Property(c => c.Id)
+			.ValueGeneratedNever();
+
+		builder.Property(c => c.CardNumber)
+			.HasConversion(
+				cn => cn.Value,
+				value => CardNumber.FromDatabase(value))
+			.HasMaxLength(16)
+			.IsRequired();
+
+		builder.HasIndex(c => c.CardNumber)
+			.IsUnique();
+
+		builder.Property(c => c.AccessLevel)
+			.HasConversion<string>()
+			.HasMaxLength(20)
+			.IsRequired();
+
+		builder.Ignore(c => c.IsExpired);
+		builder.Ignore(c => c.IsValid);
+
+		builder.HasMany<AccessLog>("_accessLogs")
+			.WithOne(l => l.AccessCard)
+			.HasForeignKey(l => l.AccessCardId)
+			.OnDelete(DeleteBehavior.Restrict);
+	}
+}
