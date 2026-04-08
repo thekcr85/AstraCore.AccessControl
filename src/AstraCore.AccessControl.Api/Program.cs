753aca4b9d52fdb8f41fd3daa7eba3246362b18e
thekcr85 <thekcr85@gmail.com>
2026-04-08 09:03:01 +0200

    Wire up endpoint registration and Scalar UI in Program.cs

--- src/AstraCore.AccessControl.Api/Program.cs
+++ src/AstraCore.AccessControl.Api/Program.cs
@@ -1,22 +1,28 @@
 using AstraCore.AccessControl.Application;
 using AstraCore.AccessControl.Infrastructure;
+using AstraCore.AccessControl.Api.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddEndpoints();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "AstraCore Access Control API";
+        options.Theme = ScalarTheme.DeepSpace;
+    });
 }
 
 app.UseHttpsRedirection();
-
-// TODO: Add endpoint mappings for all services (Employee, AccessCard, AccessPoint, AccessValidation)
-// Endpoints will be implemented in a structured Endpoints/ folder
+app.MapEndpoints();
 
 app.Run();
