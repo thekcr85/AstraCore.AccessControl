60c27a671fd4269dcf5bce8a244be0129f587f24
thekcr85 <thekcr85@gmail.com>
2026-04-08 08:07:44 +0200

    Refactor AccessCard EF configuration to use field-backed AccessLogs collection

--- src/AstraCore.AccessControl.Infrastructure/Persistence/Configurations/AccessCardConfiguration.cs
+++ src/AstraCore.AccessControl.Infrastructure/Persistence/Configurations/AccessCardConfiguration.cs
@@ -7,34 +7,39 @@ namespace AstraCore.AccessControl.Infrastructure.Persistence.Configurations;
 
 internal sealed class AccessCardConfiguration : IEntityTypeConfiguration<AccessCard>
 {
-	public void Configure(EntityTypeBuilder<AccessCard> builder)
-	{
-		builder.HasKey(c => c.Id);
+    public void Configure(EntityTypeBuilder<AccessCard> builder)
+    {
+        builder.HasKey(c => c.Id);
 
-		builder.Property(c => c.Id)
-			.ValueGeneratedNever();
+        builder.Property(c => c.Id)
+            .ValueGeneratedNever();
 
-		builder.Property(c => c.CardNumber)
-			.HasConversion(
-				cn => cn.Value,
-				value => CardNumber.FromDatabase(value))
-			.HasMaxLength(16)
-			.IsRequired();
+        builder.Property(c => c.CardNumber)
+            .HasConversion(
+                cn => cn.Value,
+                value => CardNumber.FromDatabase(value))
+            .HasMaxLength(16)
+            .IsRequired();
 
-		builder.HasIndex(c => c.CardNumber)
-			.IsUnique();
+        builder.HasIndex(c => c.CardNumber)
+            .IsUnique();
 
-		builder.Property(c => c.AccessLevel)
-			.HasConversion<string>()
-			.HasMaxLength(20)
-			.IsRequired();
+        builder.Property(c => c.AccessLevel)
+            .HasConversion<string>()
+            .HasMaxLength(20)
+            .IsRequired();
 
-		builder.Ignore(c => c.IsExpired);
-		builder.Ignore(c => c.IsValid);
+        builder.Ignore(c => c.IsExpired);
+        builder.Ignore(c => c.IsValid);
 
-		builder.HasMany<AccessLog>("_accessLogs")
-			.WithOne(l => l.AccessCard)
-			.HasForeignKey(l => l.AccessCardId)
-			.OnDelete(DeleteBehavior.Restrict);
-	}
+        // EF Core populates _accessLogs directly; AccessLogs exposes it as read-only (DDD encapsulation)
+        builder.HasMany(c => c.AccessLogs)
+            .WithOne(l => l.AccessCard)
+            .HasForeignKey(l => l.AccessCardId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Navigation(c => c.AccessLogs)
+            .HasField("_accessLogs")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
+    }
 }
